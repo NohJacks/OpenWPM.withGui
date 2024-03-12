1b6d5063f131f783d75397ffee7f0f3277cddc25
NohJacks <143488284+NohJacks@users.noreply.github.com>
2024-03-12 13:44:56 +0100

    først forsøg på at at lave funktioner til koden

--- BetterWMP.py
+++ BetterWMP.py
@@ -2,8 +2,6 @@ import argparse
 from pathlib import Path
 from typing import Literal
 
-import tranco
-
 from custom_command import LinkCountingCommand
 from openwpm.command_sequence import CommandSequence
 from openwpm.commands.browser_commands import GetCommand
@@ -11,87 +9,84 @@ from openwpm.config import BrowserParams, ManagerParams
 from openwpm.storage.sql_provider import SQLiteStorageProvider
 from openwpm.task_manager import TaskManager
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--tranco", action="store_true", default=False)
-parser.add_argument("--headless", action="store_true", default=False),
 
-args = parser.parse_args()
+class WMPcrawler:
 
-sites = [
-    "https://www.dr.dk"
-]
-if args.tranco:
-    # Load the latest tranco list. See https://tranco-list.eu/
-    print("Loading tranco top sites list...")
-    t = tranco.Tranco(cache=True, cache_dir=".tranco")
-    latest_list = t.list()
-    sites = ["http://" + x for x in latest_list.top(10)]
+    def __init__(self, sitelist):
+        self.sites = sitelist
 
+        self.display_mode: Literal["native", "headless", "xvfb"] = "native"
+        self.display_mode = "headless"
+        self.load_params()
+        self.configure_taskmanager("./datadir/", "./datadir/openwpm.log", "./datadir/crawl-data.sqlite")
 
-display_mode: Literal["native", "headless", "xvfb"] = "native"
-if args.headless:
-    display_mode = "headless"
+    def load_params(self, NUM_BROWSERS=2):
+        # Loads the default ManagerParams
+        # and NUM_BROWSERS copies of the default BrowserParams
+        NUM_BROWSERS = 2
+        self.manager_params = ManagerParams(num_browsers=NUM_BROWSERS)
+        self.browser_params = [BrowserParams(display_mode=self.display_mode) for _ in range(NUM_BROWSERS)]
 
-# Loads the default ManagerParams
-# and NUM_BROWSERS copies of the default BrowserParams
-NUM_BROWSERS = 2
-manager_params = ManagerParams(num_browsers=NUM_BROWSERS)
-browser_params = [BrowserParams(display_mode=display_mode) for _ in range(NUM_BROWSERS)]
+        # Update browser configuration (use this for per-browser settings)
+        for browser_param in self.browser_params:
+            # Record HTTP Requests and Responses
+            browser_param.http_instrument = True
+            # Record cookie changes
+            browser_param.cookie_instrument = True
+            # Record Navigations
+            browser_param.navigation_instrument = True
+            # Record JS Web API calls
+            browser_param.js_instrument = True
+            # Record the callstack of all WebRequests made
+            # browser_param.callstack_instrument = True
+            # Record DNS resolution
+            browser_param.dns_instrument = True
+            # Set this value as appropriate for the size of your temp directory
+            # if you are running out of space
+            browser_param.maximum_profile_size = 50 * (10**20)  # 50 MB = 50 * 2^20 Bytes
 
-# Update browser configuration (use this for per-browser settings)
-for browser_param in browser_params:
-    # Record HTTP Requests and Responses
-    browser_param.http_instrument = True
-    # Record cookie changes
-    browser_param.cookie_instrument = True
-    # Record Navigations
-    browser_param.navigation_instrument = True
-    # Record JS Web API calls
-    browser_param.js_instrument = True
-    # Record the callstack of all WebRequests made
-    # browser_param.callstack_instrument = True
-    # Record DNS resolution
-    browser_param.dns_instrument = True
-    # Set this value as appropriate for the size of your temp directory
-    # if you are running out of space
-    browser_param.maximum_profile_size = 50 * (10**20)  # 50 MB = 50 * 2^20 Bytes
+    def configure_taskmanager(self, datadir, logpath, storagepath):
+        # Update TaskManager configuration (use this for crawl-wide settings)
+        self.manager_params.data_directory = Path(datadir)
+        self.manager_params.log_path = Path(logpath)
+        self.storage_path = Path(storagepath)
 
-# Update TaskManager configuration (use this for crawl-wide settings)
-manager_params.data_directory = Path("./datadir/")
-manager_params.log_path = Path("./datadir/openwpm.log")
+        # memory_watchdog and process_watchdog are useful for large scale cloud crawls.
+        # Please refer to docs/Configuration.md#platform-configuration-options for more information
+        # manager_params.memory_watchdog = True
+        # manager_params.process_watchdog = True
 
-# memory_watchdog and process_watchdog are useful for large scale cloud crawls.
-# Please refer to docs/Configuration.md#platform-configuration-options for more information
-# manager_params.memory_watchdog = True
-# manager_params.process_watchdog = True
+    def run(self,):
+        # Commands time out by default after 60 seconds
+        with TaskManager(
+            self.manager_params,
+            self.browser_params,
+            SQLiteStorageProvider(self.storage_path),
+            None,
+        ) as manager:
+            # Visits the sites
+            for index, site in enumerate(self.sites):
 
+                def callback(success: bool, val: str = site) -> None:
+                    print(
+                        f"CommandSequence for {val} ran {'successfully' if success else 'unsuccessfully'}"
+                    )
 
-# Commands time out by default after 60 seconds
-with TaskManager(
-    manager_params,
-    browser_params,
-    SQLiteStorageProvider(Path("./datadir/crawl-data.sqlite")),
-    None,
-) as manager:
-    # Visits the sites
-    for index, site in enumerate(sites):
+                # Parallelize sites over all number of browsers set above.
+                command_sequence = CommandSequence(
+                    site,
+                    site_rank=index,
+                    callback=callback,
+                )
 
-        def callback(success: bool, val: str = site) -> None:
-            print(
-                f"CommandSequence for {val} ran {'successfully' if success else 'unsuccessfully'}"
-            )
+                # Start by visiting the page
+                command_sequence.append_command(GetCommand(url=site, sleep=3), timeout=60)
+                # Have a look at custom_command.py to see how to implement your own command
+                command_sequence.append_command(LinkCountingCommand())
 
-        # Parallelize sites over all number of browsers set above.
-        command_sequence = CommandSequence(
-            site,
-            site_rank=index,
-            callback=callback,
-        )
+                # Run commands across all browsers (simple parallelization)
+                manager.execute_command_sequence(command_sequence)
 
-        # Start by visiting the page
-        command_sequence.append_command(GetCommand(url=site, sleep=3), timeout=60)
-        # Have a look at custom_command.py to see how to implement your own command
-        command_sequence.append_command(LinkCountingCommand())
-
-        # Run commands across all browsers (simple parallelization)
-        manager.execute_command_sequence(command_sequence)
+if __name__ = '__main__':
+    crawler = WMPcrawler('http://www.dr.dk')
+    crawler.run()
